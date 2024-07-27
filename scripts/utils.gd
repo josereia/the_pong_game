56526bce1fdf589809a6574650933184e5653f31
João Sereia <joao.lo.sereia@gmail.com>
2024-07-27 12:28:46 -0300

    feat(scripts/utils): add get and set game location methods

--- scripts/utils.gd
+++ scripts/utils.gd
@@ -3,3 +3,25 @@ class_name Utils
 static func getGameVersion() -> String:
 	var version = ProjectSettings.get_setting("application/config/version")
 	return version
+
+static func setLocale(locale: Locales) -> void:
+	var language: String
+	if (locale == Locales.EN):
+		language = 'en'
+	elif (locale == Locales.PT):
+		language = 'pt'
+
+	TranslationServer.set_locale(language)
+
+static func getLocale() -> Locales:
+	var language: String = TranslationServer.get_locale()
+
+	var locale: Locales
+	if (language.contains('en')):
+		locale = Locales.EN
+	elif (language.contains('pt')):
+		locale = Locales.PT
+
+	return locale
+	
+enum Locales {EN, PT}
