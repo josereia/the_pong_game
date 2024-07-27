79a56b39196c0419d2424deec9f44f343be7a863
João Sereia <joao.lo.sereia@gmail.com>
2024-07-27 00:40:40 -0300

    feat(scenes/main): replace the label credits with the link button for github

--- scripts/main.gd
+++ scripts/main.gd
@@ -3,9 +3,6 @@ extends Node2D
 func _ready() -> void:
 	$Version.text = getGameVersion()
 
-func _process(delta: float) -> void:
-	pass
-
 func getGameVersion() -> String:
 	var version = ProjectSettings.get_setting("application/config/version")
 	return version
