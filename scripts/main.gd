77c764b2e10cb03a091ebdd9d0be0879478c9c59
João Sereia <joao.lo.sereia@gmail.com>
2024-07-28 02:36:50 -0300

    feat(scenes/game): add game scene without functionality

--- scripts/main.gd
+++ scripts/main.gd
@@ -3,8 +3,11 @@ extends Control
 func _ready() -> void:
 	$Version.text = Utils.getGameVersion()
 
-func _on_credits_button_pressed() -> void:
-	get_tree().change_scene_to_file('res://scenes/credits.tscn')
+func _on_start_button_pressed() -> void:
+	get_tree().change_scene_to_file('res://scenes/game.tscn')
 
 func _on_settings_button_pressed() -> void:
 	get_tree().change_scene_to_file('res://scenes/settings.tscn')
+
+func _on_credits_button_pressed() -> void:
+	get_tree().change_scene_to_file('res://scenes/credits.tscn')
